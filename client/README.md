baf9b6bf8cb5374b11c4fa23a06b48dc960c43c7
Anshul Raheja <anshul.r1610@gmail.com>
2021-09-05 03:49:47 +0530

    initial commit

---
+++ client/README.md
@@ -0,0 +1,19 @@
+# Crypto Currencency Data Tracker 
+
+## Decription 
+It displays the price, percent change, market value, and volumne of 100 cryptocurrencies. 
+Users can also search a particular currency to view its data.
+
+![screenshot](/client/public/images/cryptoCurrenyDataTracker_github_img.jpg)
+
+### My learnings 
+1. Axios: Javascript library used to make HTTP requests to REST endpoints and perform CRUD operations. I used Axios to get data from the [API](https://www.coingecko.com/en/api).
+
+### Tech Stack
+React, CSS 
+
+## Installation 
+```bash
+git clone https://github.com/anshulraheja/crypto-currency-data-tracker.git
+npm install
+```
---
+++ client/src/App.js
@@ -0,0 +1,52 @@
+import './App.css';
+import  {useState, useEffect} from 'react';
+import axios from 'axios'
+import Coin from './components/Coin';
+
+const URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=24h';
+
+function App() {
+
+  //array of all the row data 
+  const [coins, setCoins] = useState([])
+
+  //state variable for input box
+  const [search, setSearch] = useState('')
+
+  //fetching data from URL on page load
+  useEffect(() => {
+    axios.get(URL)
+    .then(res => {
+      setCoins(res.data)
+    }).catch(error => alert('Error in fetching data. Try again!'));
+  }, []);
+
+  //setting search variable to input text
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  }
+
+  //filter table rows according to crypto name or sybmol input
+  const coinList = coins.filter(coin => 
+    coin.name.toLowerCase().includes(search.toLowerCase()) || coin.symbol.toLowerCase().includes(search.toLowerCase())
+  )
+
+  return (
+    <div className="coin-app">
+      <div className="coin-container">
+        <h1 className="title">Cryptocurrency Data Tracker</h1>
+        <form>
+          <input 
+          type="text" 
+          placeholder="Bitcoin" 
+          className="coin-input"
+          onChange={handleChange} />
+        </form>
+      </div>
+      <Coin coinList={coinList} />
+    </div>
+  );
+}
+
+export default App;
